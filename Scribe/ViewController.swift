fc34c18fead1455f7d31a281b96c996e4e31ce21
aabiro <aaryn.alexander@gmail.com>
2017-05-24 12:33:23 -0400

    Initial Commit

---
+++ Scribe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scribe
+//
+//  Created by Maureen Biro on 2017-05-24.
+//  Copyright © 2017 Aaryn Biro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
